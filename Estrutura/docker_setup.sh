760795eff367f5d36ef33cd73b506d7f172b4164
Ronynetwork <ronyldooliveira16@gmail.com>
2024-08-13 00:10:35 -0300

    adicionando -y para confirmacao direta

--- Estrutura/docker_setup.sh
+++ Estrutura/docker_setup.sh
@@ -19,4 +19,4 @@ echo \
 
 # Atualiza a lista de pacotes e instala o Latest dos plugins docker
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
