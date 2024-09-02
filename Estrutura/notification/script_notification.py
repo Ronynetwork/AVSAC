4677c720eaf8be553444d63b3cefe14613ab2ad1
Ronynetwork <ronyldooliveira16@gmail.com>
2024-09-02 14:35:47 -0300

    comentando codigo e adicionando lambda que filtra os erros apenas pro do project key

--- Estrutura/notification/script_notification.py
+++ Estrutura/notification/script_notification.py
@@ -13,21 +13,20 @@ try:
         auth=(TOKEN, '')
     )
     arq = response.json()
-    
+    filtred_ussues = [issue for issue in arq.get('issues', []) if issue['project'] == PROJECT_KEY]
     # Separando o retorno da requisição
-    for issue in arq.get('issues', []):
-        issue_id = issue['key']
-        message = issue['message']
-        severity = issue['severity']
-        line = int(issue['textRange']['startLine'])
-        component = issue['component']
-        if component not in components_msg:
+    for issue in filtred_ussues:
+        message = issue['message'] # Atribuindo mensagem de erro
+        severity = issue['severity'] # Atribuindo severidade
+        line = int(issue['textRange']['startLine']) # Atribuindo linha
+        component = issue['component'] # Atribuindo Path
+        if component not in components_msg: # Fazendo verificação se o componente ja existe no dicionario, se nao adiciona como novo dicionario na lista
             components_msg[component] = [{
             "messages": message,
             "severity": severity,
             "line": line,
             }]
-        else:
+        else: # se sim, adiciona a mensagem com um append
             components_msg[component].append({
                 "messages": message,
                 "severity": severity,
@@ -37,26 +36,26 @@ try:
 except Exception as e:
     print(f"Erro ao obter problemas do projeto: {e}")
 
-# Dados de exemplo (substitua pelos dados reais do seu script)
+# definindo lista com erros
 errors = []
-for path in components_msg.keys():
+for path in components_msg.keys(): # Fazendo for a cada chave do dicionario
     try:
         response_code = requests.get(
-            f"{SONARQUBE_URL}/api/sources/raw?key={path}",
+            f"{SONARQUBE_URL}/api/sources/raw?key={path}", # Fazendo get no codigo que foi submetido a análise
             auth=(TOKEN, '')
         )
-        code = response_code.text.splitlines()
-        path_split = path.replace(f'{PROJECT_KEY}:', '')
+        code = response_code.text.splitlines() # Retornando a resposta em uma lista de linhas do arquivo
+        path_split = path.replace(f'{PROJECT_KEY}:', '') # Alterando o path do arquivo para remover o prefixo do projeto no sonar, deixando apenas o path limpo
         
         # Adicionando os erros à lista
-        details = components_msg[path]
-        for det in details:
+        details = components_msg[path] # Definindo details como o conteudo daquele path do dicionario, ou seja, as mensagens
+        for det in details: # para cada detalhe nos details eu crio um dicionario com as diretivas semelhantes ao do for anterior, porem com o path atualizado e com a linha de codigo
             error_entry = {
                 "file": path_split,
                 "message": det['messages'],
                 "severity": det['severity'],
                 "line": det['line'],
-                "code": code[det['line'] - 1] if det['line'] - 1 < len(code) else ""
+                "code": code[det['line'] - 1] if det['line'] - 1 < len(code) else "" # fazendo um tratamento lambda para buscar no codigo a linha de erro -1 o que retorna a linha exata do erro
             }
             errors.append(error_entry)
     except Exception as e:
@@ -79,8 +78,8 @@ html_content = '''
 '''
 
 # Itera sobre a lista de erros para adicionar ao HTML
-for index, error in enumerate(errors):
-    html_content += f'''
+for index, error in enumerate(errors): # Faço um for de chave,valor e enumero a quantidade de casos do dicionario para ocorrer de forma certa, depois dou apprend nos pontos especificos
+    html_content += f''' 
         <div class="error" id="error{index + 1}">
             <h3 class="error-title">{error["file"]} <span class="toggle-icon"></span></h3>
             <div class="error-details">
@@ -91,7 +90,7 @@ for index, error in enumerate(errors):
         </div>
     '''
 
-# Fechando as tags HTML
+# Fechando as tags HTML e indicando arquivo js
 html_content += '''
     </div>
     <script src="script_java.js"></script>
