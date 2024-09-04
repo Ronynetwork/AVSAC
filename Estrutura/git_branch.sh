0fdaeb48e6c4ce189766e3ba9a8c9bd1a6a6bdb9
Ronynetwork <ronyldooliveira16@gmail.com>
2024-09-04 17:58:06 -0300

    removendo diretivas desnecessarias

--- Estrutura/git_branch.sh
+++ Estrutura/git_branch.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-mv package-lock.json package.json /tmp/
 # conferindo se branch main existe, se nao, cria
 git pull --rebase origin main
 git checkout -b main || git checkout main
 # Configurar usuário e e-mail do Git
 git pull --rebase origin main
-git add ./test_scripts/teste.py
+git add ./teste_scripts/teste.py
 git commit -m "Correction commit"
 git push origin main
 
