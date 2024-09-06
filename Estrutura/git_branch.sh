cf86703a3f38ecf3eea986e450a8122934ab9c56
Ronynetwork <ronyldooliveira16@gmail.com>
2024-09-06 00:21:25 -0300

    alterando verificação e otimizando codigo

--- Estrutura/git_branch.sh
+++ Estrutura/git_branch.sh
@@ -2,18 +2,8 @@
 
 # Conferindo se a branch main existe, se não, cria
 git reset --hard
-git pull
 git checkout -b main || git checkout main
-
-# Restaurar todos os arquivos modificados para o estado do último commit
-git restore --staged .
-git restore .
-
-# Limpar arquivos não rastreados
-git clean -fd
-
-# Configurar usuário e e-mail do Git
-# (certifique-se de que essas configurações já estão feitas globalmente ou localmente)
+git pull
 
 # Adicionar apenas o arquivo desejado
 git add ./teste_scripts/teste.py
